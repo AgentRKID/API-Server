9adde960870f4fc0a1c372201c51f7edfc87a7f3
AgentRKID <rbuhmc@gmail.com>
2021-07-18 08:36:34 -0700

    Add prefixes

--- Common/src/main/java/cc/nuplex/api/common/RankType.java
+++ Common/src/main/java/cc/nuplex/api/common/RankType.java
@@ -1,7 +1,14 @@
 package cc.nuplex.api.common;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum RankType {
-    OWNER,
-    DEV,
-    DEFAULT
+    OWNER("&7[&cOwner&7] &c"),
+    DEV("&7[&bDev&7] &b"),
+    DEFAULT("&7");
+
+    private final String prefix;
 }
